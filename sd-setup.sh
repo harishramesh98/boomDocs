5526e2f47d0d1d8a6bf7db19becd4fc69c32f9c6
Audrey Yang <29296576+audrey-yang@users.noreply.github.com>
2023-07-18 17:23:08 -0400

    Fix sd-setup.sh
    Alignment fix + echoes

--- sd-setup.sh
+++ sd-setup.sh
@@ -2,23 +2,29 @@
 # Note: Assumes the SD card is /dev/sdb (run `lsblk` to verify)
 
 # Zap everything
+echo "ZAP"
 sgdisk -Z /dev/sdb
+echo
 
 # Create new empty GPT
+echo "Empty GPT"
 sgdisk -o /dev/sdb 
+echo 
 
 # Create boot partition
-sgdisk -a 34 /dev/sdb
-sgdisk -n 1:34:1048610 /dev/sdb
+echo "BOOT PARTITION"
+sgdisk -a 34 -n 1:34:1048610 /dev/sdb
 sgdisk -t 1:a800 /dev/sdb # set GUID to Apple UFS
+echo
 
 # Create root partition
-sgdisk -n 2:1048628:15118302 /dev/sdb
+echo "ROOT PARTITION"
+sgdisk -a 34 -n 2:1048628:15118302 /dev/sdb
 sgdisk -t 2:af00 /dev/sdb # set GUID to Apple HFS
+echo 
 
 # Set up file sstem for root partition
 sudo mkfs.hfs -v "Prototype Data" /dev/sdb2
 
 # Copy Linux binary to boot partition
 sudo dd if=~/boom/br-base-bin-nodisk-flat of=/dev/sdb1
-
