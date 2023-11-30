eb98003645bd93a060a2c7a0ad6e88b83d90e075
Harish Ramesh <38978733+harishramesh98@users.noreply.github.com>
2023-11-30 13:36:09 -0500

    Update testFileMain.c with testCase3

--- TestBench/testFileMain.c
+++ TestBench/testFileMain.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdint.h>
-#include<math.h>
+#include <math.h>
 #include <stdlib.h>
 #include "hpm-util.h"
 
 //test case function declarations
 void testCase1();
 void testCase2();
-
+void testCase3();
 
 int main(int argc, char **argv){
 	if(argc>1){
@@ -15,6 +15,8 @@ int main(int argc, char **argv){
 			case 1: testCase1();
 				break;
 			case 2: testCase2();
+				break;
+			case 3: testCase3();
 				break;
 			default: printf("test case undefined\n");
 				break;
@@ -33,14 +35,14 @@ void testCase1(void){
 	printf("cycle(%lu)\n", start_cycles);
     	printf("instr(%lu)\n", start_instr);
     	
-    	int a, b;
-    	float c;
+    	int a,b;
+	float c;
     	printf("Enter side A:\n");
     	scanf("%d",&a);
     	printf("enter side B:\n");
     	scanf("%d",&b);
     	c=sqrt(a*a+b*b);
-    	printf("Hypotenuse is %f units", c);
+    	printf("Hypotenuse is %f units\n",c);
     	
     	uint64_t end_cycles = HPM_READ(cycle);
 	uint64_t end_instr = HPM_READ(instret);
@@ -85,3 +87,29 @@ void testCase2(void){
 return;
 }
 
+void testCase3(void){
+	    printf("Testing all, with removed scounteren and problematic stateent commentd out\n");
+
+	        // setup hpm events
+		//can only use mhpmevent* here
+		HPM_SETUP_EVENTS(mhpmevent3, 0, 1);
+		HPM_SETUP_EVENTS(mhpmevent4, 0, 2);
+		// HPM_SETUP_EVENTS(mhpmevent5, 0, 4);
+		printf("started!\n");
+		// read hpms
+		// can use either mhpm... or hpm... here depending on if you enable or user mode / supervisor mode reading
+		printf("cycle(%lu)\n", HPM_READ(cycle));
+		printf("instr(%lu)\n", HPM_READ(instret));
+		// note: have to "zero" them out since they are not zero to begin with
+		uint64_t start_hpm3 = HPM_READ(mhpmcounter3);
+		uint64_t start_hpm4 = HPM_READ(mhpmcounter4);
+		//uint64_t start_hpm5 = HPM_READ(mhpmcounter5);
+		printf("stalling\n");
+		uint64_t end_hpm3 = HPM_READ(mhpmcounter3);
+		uint64_t end_hpm4 = HPM_READ(mhpmcounter4);
+		// uint64_t end_hpm5 = HPM_READ(mhpmcounter5);
+		printf("%lu = %lu - %lu\n", end_hpm3 - start_hpm3, end_hpm3, start_hpm3);
+		printf("%lu = %lu - %lu\n", end_hpm4 - start_hpm4, end_hpm4, start_hpm4);
+		//printf("%lu = %lu - %lu\n", end_hpm5 - start_hpm5, end_hpm5, start_hpm5);
+		return;
+		}
